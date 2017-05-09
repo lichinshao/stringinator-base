62eee6d0fbffc035a7dca2a4ddb3fa389cb2c50d
lichinshao <lichinshao@gmail.com>
2017-05-09 13:09:37 -0700

    map-underbar.js

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,25 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(_.isArrayLike(array)).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const obj = {
+      length: 5,
+      4: 8, 
+      what: 'the heck'
+    };
+    expect(_.isArrayLike(obj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const obj = {
+      length: 'pony',
+      4: 8, 
+      what: 'the heck'
+    }
+    expect(_.isArrayLike(obj)).toBe(false);
   });
 });
--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -2,6 +2,7 @@ const _ = require('../../underbar');
 
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5];
+    expect(_.map(array, item => item * item)).toEqual([1, 4, 9, 16, 25]);
   });
 });
