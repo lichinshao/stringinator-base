b2a4de7187754848a11e88f593e7e79e56332280
lichinshao <lichinshao@gmail.com>
2017-05-09 11:54:20 -0700

    underbar/indexOf

--- test/underbar/indexOf.test.js
+++ test/underbar/indexOf.test.js
@@ -2,23 +2,28 @@ const _ = require('../../underbar');
 
 describe('indexOf()', () => {
   it('returns the index of a value at the beginning of an array', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(_.indexOf(array, 1)).toEqual(0);
   });
 
   it('returns the index of a value at the end of an array', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(_.indexOf(array, 6)).toEqual(5);
   });
 
   it('returns -1 for a missing value', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(_.indexOf(array, 8)).toEqual(-1);
   });
 
   it('returns the first matching index when multiple matches in array', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6, 2, 3, 4, 2, 2, 3];
+    expect(_.indexOf(array, 3)).toEqual(2);
   });
 
   it('starts searching at the given offset', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5, 6, 7, 5, 3, 2, 4];
+    expect(_.indexOf(array, 5, 3)).toBe(4);
   });
 
 });
